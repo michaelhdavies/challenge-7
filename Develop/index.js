1ba6e7d41e9909ad862421ba237a43d27c302fcb
Michael Henry Davies <michael_methodtheatre.com>
2025-01-21 12:02:41 -0700

    inquirer prompts working

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,78 @@
+
+// 'Description: ',
+// 'Table of Contents: ',
+// 'Installation: ',
+// 'Usage: ',
+// 'License: ',
+// 'Contributing: ',
+// 'Tests: ',
+// 'Questions: ',
+
 // TODO: Include packages needed for this application
+import fs from 'fs';
+import inquirer from 'inquirer';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+	{
+		type: 'input',
+		message: 'Please submit the title of your application: ',
+		name: 'title',
+	},
+	{
+		type: 'input',
+		message: 'Please (briefly) describe your application: ',
+		name: 'description',
+	},
+	{
+		type: 'input',
+		message: 'Please describe the installation process: ',
+		name: 'install',
+	},
+	{
+		type: 'input', 
+		message: 'Please describe the usage/functionality: ',
+		name: 'usage',
+	},
+	{
+		type: 'list',
+		message: 'Please select a license from the following options: ',
+		name: 'license',
+		choices: ['License 1', 'License 2', 'License 3'],
+	},
+	{
+		type: 'input',
+		message: 'Please outline contribution guidelines: ',
+		name: 'contributions',
+	},
+	{
+		type: 'input',
+		message: 'Please describe the testing process for this application: ',
+		name: 'tests'
+	},
+	{
+		type: 'input',
+		message: 'Please enter your github username: ',
+		name: 'github',
+	},
+	{
+		type: 'input',
+		message: 'Please enter your email: ',
+		name: 'email',
+	},
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { };
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+	inquirer.prompt(questions).then((response) => {			// Inquire
+		console.log(response);
+		return(response)}
+	)
+};
 
 // Function call to initialize app
 init();
